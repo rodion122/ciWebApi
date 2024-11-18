c91335ee50e79fb910a31795c3632003cd4a6fd5
IRodon <57728693+rodion122@users.noreply.github.com>
2024-11-18 14:55:48 +0300

    Addded project

---
+++ src/main/java/org/example/ciwebapiexample/CiWebApiExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.ciwebapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiWebApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiWebApiExampleApplication.class, args);
+    }
+
+}
